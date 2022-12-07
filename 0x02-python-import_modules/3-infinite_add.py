0a55e8717eaae14cc0c8229ddbc0fd045c2bdca7
eze blessing <blessingchiaha@yahoo.com>
2022-12-07 05:03:34 -0800

    finished and commited

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
-#!/usr/bin/bash/python3
-import sys
+#!/usr/bin/python3
 
-if __name__ == '__main__':
-    argv = sys.argv[1:]
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
     total = 0
-    for number in argv;
-        total +-
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
