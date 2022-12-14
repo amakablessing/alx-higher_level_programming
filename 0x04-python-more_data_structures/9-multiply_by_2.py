c870cced4ed1e7025f20ee10bbf979f1b907d4b3
eze blessing <blessingchiaha@yahoo.com>
2022-12-13 23:18:30 -0800

    function that computes the square value of all integers of a matrix and rest of others

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,10 @@
 
 
 def multiply_by_2(a_dictionary):
+    """
+    a function that returns a new
+    dictionary with all values multiplied by 2
+    """
      new_dict = {}
      for key, value in a_dictionary.items():
          new_dict.update({key: (value * 2)})
