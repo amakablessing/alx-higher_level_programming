74addc5e0246cd962df6320c89288e85f3196a00
eze blessing <blessingchiaha@yahoo.com>
2022-12-18 23:16:07 -0800

    a function that prints all integers of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+    def print_list_integer(my_list=[]):
+        for i in range(len(my-list)):
+            print("{:d}".format(my-list[i]))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+This directory entails on Structures: Lists, Tuples
